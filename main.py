0c46a653ac9fb4a0c4a92fc562ca90ff86c344b3
miguelmonterodev <miguelmonterodev@gmail.com>
2025-08-27 09:34:57 +0100

    create asteroids

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -14,9 +16,15 @@ def main():
     # Creating Groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroid = pygame.sprite.Group()
+    # Every instance of the class is added to these groups upon creation
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroid, updatable, drawable) 
+    AsteroidField.containers = (updatable)
     # Player
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    # AsteroidField
+    asteroid_field = AsteroidField()
     # Game Loop
     while True:
         # Check if the user has closed the window and exit the game loop 
